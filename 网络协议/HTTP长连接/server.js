f2cb2f543a77e1146f7936aef53587a91102b800
suagnes <suagnes@163.com>
2019-02-18 17:27:49 +0800

    0219

--- 网络协议/HTTP长连接/server.js
+++ 网络协议/HTTP长连接/server.js
@@ -8,13 +8,14 @@ http.createServer(function(request,response){
     if(request.url === '/'){
         response.writeHead(200,{
             'Content-Type':'text/html',
-            'Connection':'close'
+            // 'Connection': keep-alive,
+            // 'Connection':'close'
         })
     response.end(html);
 }else{
         response.writeHead(200,{
             'Content-Type':'image/png',
-            'Connection':'close'
+            // 'Connection':'close'
         })
     response.end(img);
     }
@@ -22,14 +23,14 @@ http.createServer(function(request,response){
 console.log('server listening on 8892');
 
 // 长连接会复用TCP/IP的连接
-// 默认情况下Connection: keep-alive
+// 默认情况下Connection: keep-alive : 这个连接是被保持的,不会一下子请求完成后就被关闭
 // Connection: close 一个请求完毕后TCP连接就会被关闭,每一个Connection ID都是不同的(没有复用TCP连接,每次请求发送完成后,TCP连接都被关闭了)
 // 正常情况下HTTP服务都是利用Connection: keep-alive , 我们可以给Connection: keep-alive设置一个自动关闭的时间,但是这个是在服务端设置操作的,本身和HTTP协议没有什么关系,并不会在http头上有什么展示
 
 // 在HTTP2里出现了一个新的概念叫信道复用:
 // 在TCP连接上可以并发的发送HTTP请求
 // 也就是说我们连接一个网站的时候只需要一个TCP连接
-// GOOGLE已经都是HTTP2了 所有的都是一个TCP连接(仅限同域) 开销降低 速度提升
+// GOOGLE.com已经都是HTTP2了 所有的都是一个TCP连接(仅限同域) 开销降低 速度提升
 
 // 长连接:
 // 理解HTTP请求是在TCP连接上进行发送的,一个TCP连接可以发送多个HTTP请求
--- 网络协议/数据协商/server.js
+++ 网络协议/数据协商/server.js
@@ -16,4 +16,16 @@ http.createServer(function(request,response){
 console.log('server listening on 8893');
 // Size里上面是传输信息 下面是body信息
 // 压缩可以加快网络传输的开销
+
+// 数据协商:在客户端发送给服务端一个请求的时候,客户端会声明这个请求希望拿到的数据格式以及具体相关的一些限制都是怎么样的
+// 服务端会根据它的请求里表示的数据,做出判断,可能会返回不同类型的数据。
+// Accpet: 指定想要的数据类型
+// Accpet-Encoding: 代表数据以怎么样的编码方式来进行传输,主要来限制服务端如何进行一个数据的压缩
 // 可以根据Accpet-Encoding去选择认为最好的压缩算法
+// Accpet-Language: 判断语言
+// User-Agent: 表示浏览器相关信息
+
+// 服务端返回的
+// Content-Type: 返回的数据格式
+// Content-Encoding: 返回数据压缩方式
+// Content-Language: 返回语言
